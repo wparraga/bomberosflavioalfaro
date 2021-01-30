7e29838e9f6b7322aec9c51b1f1f2f7a2b108fba
wparraga <walterdaniel15@hotmail.com>
2021-01-29 21:54:44 -0500

    realizado hasta buscar permisos de funcionamiento

--- php/ajax/permiso_funcionamiento/buscar_permiso.php
+++ php/ajax/permiso_funcionamiento/buscar_permiso.php
@@ -7,8 +7,8 @@
 	$action = (isset($_REQUEST['action'])&& $_REQUEST['action'] !=NULL)?$_REQUEST['action']:'';
 	if($action == 'ajax'){
          $q = mysqli_real_escape_string($con,(strip_tags($_REQUEST['q'], ENT_QUOTES)));
-		 $aColumns = array('perfun_fechaemision', 'perfun_persona', 'perfun_cedularuc');
-		 $sTable = "permiso_funcionamiento";
+		 $aColumns = array('per_propietario', 'per_ruc', 'per_local');
+		 $sTable = "permisos_bomberos";
 		 $sWhere = "";
 		if ( $_GET['q'] != "" )
 		{
@@ -20,7 +20,7 @@
 			$sWhere = substr_replace( $sWhere, "", -3 );
 			$sWhere .= ')';
 		}
-		$sWhere.=" order by perfun_codigo desc";
+		$sWhere.=" order by per_codigo desc";
 		include '../../../views/pagination.php'; 
 		
 		$page = (isset($_REQUEST['page']) && !empty($_REQUEST['page']))?$_REQUEST['page']:1;
@@ -45,37 +45,47 @@
 		        <tr class="success">
 		            <th>ID</th>
 		            <th>Emisión</th>
-		            <th>Nombres</th>
-		            <th>Cédula/RUC</th>
-		            <th>Actividad</th>
-		            <th>Válido</th>
-		            <th><span class="pull-right">Acciones</span></th>
+		            <th>Nro.</th>
+		            <th>Propietario</th>
+		            <th>Local</th>
+		            <th>Dirección</th>
+		            <th>Per_Fun</th>
+		            <th>Per_Ins</th>
+		            <th>Año</th>
+		            <th></th>
 
 		        </tr>
 		        <?php
 						while ($row=mysqli_fetch_array($query)){
-								$perfun_codigo=$row['perfun_codigo'];
-								$perfun_fechaemision=$row['perfun_fechaemision'];
-								$perfun_persona=$row['perfun_persona'];
-								$perfun_cedularuc=$row['perfun_cedularuc'];
-								$perfun_actividad=$row['perfun_actividad'];
-								$perfun_ubicacion=$row['perfun_ubicacion'];
-								$perfun_valido=$row['perfun_valido'];
-								$perfun_usuario=$row['perfun_usuario'];
+								$per_cod=$row['per_codigo'];
+								$per_fec=$row['per_fechaemision'];
+								$per_num=$row['per_numero'];
+								$per_loc=$row['per_local'];
+								$per_pro=$row['per_propietario'];
+								$per_ruc=$row['per_ruc'];
+								$per_dir=$row['per_direccion'];
+								$per_tel=$row['per_telefono'];
+								$per_tasapermisofuncionamiento=$row['per_tasapermisofuncionamiento'];
+								$per_tasapermisoinspeccion=$row['per_tasapermisoinspeccion'];
+								$per_anio=$row['per_anio'];
+								$per_usuariogenera=$row['per_usuariogenera'];
 							?>
 
-		        <input type="hidden" value="<?php echo $row['perfun_persona'];?>" id="perfun_persona<?php echo $perfun_codigo;?>">
-		        <input type="hidden" value="<?php echo $row['perfun_cedularuc'];?>" id="perfun_cedularuc<?php echo $perfun_codigo;?>">
-		        <input type="hidden" value="<?php echo $row['perfun_actividad'];?>" id="perfun_actividad<?php echo $perfun_codigo;?>">
-		        <input type="hidden" value="<?php echo $row['perfun_ubicacion'];?>" id="perfun_ubicacion<?php echo $perfun_codigo;?>">
-		        <input type="hidden" value="<?php echo $row['perfun_valido'];?>" id="perfun_valido<?php echo $perfun_codigo;?>">
+		        <input type="hidden" value="<?php echo $row['perfun_persona'];?>" id="perfun_persona<?php echo $per_cod;?>">
+		        <input type="hidden" value="<?php echo $row['perfun_cedularuc'];?>" id="perfun_cedularuc<?php echo $per_cod;?>">
+		        <input type="hidden" value="<?php echo $row['perfun_actividad'];?>" id="perfun_actividad<?php echo $per_cod;?>">
+		        <input type="hidden" value="<?php echo $row['perfun_ubicacion'];?>" id="perfun_ubicacion<?php echo $per_cod;?>">
+		        <input type="hidden" value="<?php echo $row['perfun_valido'];?>" id="perfun_valido<?php echo $per_cod;?>">
 		        <tr>
-		            <td><?php echo $perfun_codigo; ?></td>
-		            <td><?php echo $perfun_fechaemision; ?></td>
-		            <td><?php echo $perfun_persona; ?></td>
-		            <td><?php echo $perfun_cedularuc; ?></td>
-		            <td><?php echo $perfun_actividad;?></td>
-		            <td><?php echo $perfun_valido;?></td>
+		            <td><?php echo $per_cod; ?></td>
+		            <td><?php echo $per_fec; ?></td>
+		            <td><?php echo $per_num; ?></td>
+		            <td><?php echo $per_pro;?></td>
+		            <td><?php echo $per_loc;?></td>
+		            <td><?php echo $per_dir;?></td>
+		            <td><?php echo '$'.$per_tasapermisofuncionamiento;?></td>
+		            <td><?php echo '$'.$per_tasapermisoinspeccion;?></td>
+		            <td><?php echo $per_anio;?></td>
 
 		            <td><span class="pull-right">
 
